ae5a704cb2934901fa3bbe9ac7995e8a0dc815bd
kristof <szabokristof0@gmail.com>
2019-01-18 14:19:35 +0100

    Contact page

--- personal/urls.py
+++ personal/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include, re_path
 from . import views
 
 urlpatterns = [
-    re_path('', views.index, name='index'),
+    re_path('contact/$', views.contact, name='contact'),
+    re_path('', views.index, name='index'),   
 ]
