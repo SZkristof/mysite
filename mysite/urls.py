a012d2eb8bf18a4fa80bdc29d0e8e0772f3bb37d
kristof <szabokristof0@gmail.com>
2019-01-18 15:45:19 +0100

    Urls.py test

--- mysite/urls.py
+++ mysite/urls.py
@@ -18,7 +18,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('blog/', include('blog.urls')),
     path('webapp/', include('webapp.urls')),
+    path('blog/', include('blog.urls')),
     path('', include('personal.urls')),
 ]
